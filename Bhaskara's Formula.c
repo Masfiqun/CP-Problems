1fa645b106b89dd56c7c3c2236b522dd00f434ae
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-02-25 00:33:38 +0600

    new one

--- Bhaskara's Formula.c
+++ Bhaskara's Formula.c
@@ -12,3 +12,26 @@ Output
 Print the result with 5 digits after the decimal point or the message if it is impossible to calculate.
 */
 
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double A, B, C, R1, R2, delta;
+
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    delta = B*B - 4*A*C;
+
+    if(A == 0 || delta < 0) {
+        printf("Impossivel calcular\n");
+    }
+    else {
+        R1 = (-B + sqrt(delta)) / (2*A);
+        R2 = (-B - sqrt(delta)) / (2*A);
+        printf("R1 = %.5lf\n", R1);
+        printf("R2 = %.5lf\n", R2);
+    }
+
+    return 0;
+}
+
