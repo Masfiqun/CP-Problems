5633f5275e0b26a14de891d235f0cee7829163b7
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-02-26 20:34:48 +0600

    tour

--- sncak.c
+++ sncak.c
@@ -14,3 +14,27 @@ Output
 The output must be a message "Total: R$ " followed by the total value to be paid, with 2 digits after the decimal point.
 */
 
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int X, Y;
+
+    scanf("%d %d", &X, &Y);
+
+    if(X == 1){
+        printf("Total: R$ %.2lf\n", Y*4.00);
+    }else if(X == 2){
+        printf("Total: R$ %.2lf\n", Y*4.50);
+    }else if(X == 3){
+        printf("Total: R$ %.2lf\n", Y*5.00);
+    }else if(X == 4){
+        printf("Total: R$ %.2lf\n", Y*2.00);
+    }else if(X == 5){
+        printf("Total: R$ %.2lf\n", Y*1.50);
+    }else{
+        printf("not applicable\n");
+    }
+
+    return 0;
+}
