0e2c20aefea3bb66c053dd7a3afe756e0d867f66
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-03-01 02:40:40 +0600

    sort

---
+++ Simple Sort.c
@@ -0,0 +1,14 @@
+/*beecrowd | 1042
+Simple Sort
+Adapted by Neilor Tonin, URI  Brazil
+
+Timelimit: 1
+Read three integers and sort them in ascending order. After, print these values in ascending order, a blank line and then the values in the sequence as they were readed.
+
+Input
+The input contains three integer numbers.
+
+Output
+Present the output as requested above.
+*/
+
